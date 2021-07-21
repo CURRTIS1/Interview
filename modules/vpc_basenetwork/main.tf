81c3dcb7093fc587b44f32b3f350fa868c1eb74c
unknown <curtiswallond@gmail.com>
2021-07-21 19:55:10 +0100

    removed leading line in policy json

--- modules/vpc_basenetwork/main.tf
+++ modules/vpc_basenetwork/main.tf
@@ -46,15 +46,15 @@ data "aws_availability_zones" "available" {
 resource "aws_iam_role" "ssm_role" {
   name               = "ssm_role"
   assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": {
-        "Effect": "Allow",
-        "Principal": {"Service": "ec2.amazonaws.com"},
-        "Action": "sts:AssumeRole"
-      }
-    }
-  EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Principal": {"Service": "ec2.amazonaws.com"},
+    "Action": "sts:AssumeRole"
+  }
+}
+EOF
 }
 
 resource "aws_iam_role_policy_attachment" "ssmrole_attach" {
